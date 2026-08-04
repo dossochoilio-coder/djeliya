ab1d6fe7859dbe24de5add6e9ecae6eb7d2e1daf
Doscharl <dosso.choilio@gmail.com>
2026-08-04 16:16:59 +0100

    Add files via upload

---
+++ mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.djeliya.app',
+  appName: 'Djeliya',
+  webDir: 'dist'
+};
+
+export default config;
